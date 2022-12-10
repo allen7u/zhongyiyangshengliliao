d90c6e2e25c75508bbcb080b5be30dbcbf10dcd7
allen7u@GitKraken <allen7u@gmail.com>
2022-12-10 16:19:12 +0800

    add build request pending and create request abort

--- main.js
+++ main.js
@@ -9,6 +9,10 @@
 
 const puppeteer = require('puppeteer');
 
+var sell_time = new Date(2022,11,9,23,18,00,000).getTime();
+var seconds_ahead = 1
+var load_time = sell_time - seconds_ahead * 1000;
+
 (async()=>{
     const browserURL = 'http://localhost:9222'
     let browser=await puppeteer.connect({ 
@@ -23,27 +27,45 @@ const puppeteer = require('puppeteer');
     await page.setRequestInterception(true)
 
     page.on('request', (request) => {        
-    console.log('>>', print_current_time(), request.method(), request.url().slice(0, 70))
-    request.continue()
-    })
+    if(request.url().includes('trade.order.build')) {
+        var pending = setInterval(() => {
+            if ( Date.now() > sell_time ) {
+                request.continue();
+                console.log('=>', print_current_time(), request.method(), request.url().slice(0, 70))
+                clearInterval(pending);
+            }
+        }, 10);
+    } else if (request.url().includes('trade.order.create')) {
+        request.abort();
+        console.log('<>', print_current_time(), request.method(), request.url().slice(0, 70))
+    } else {
+        request.continue()
+        console.log('>>', print_current_time(), request.method(), request.url().slice(0, 70))
+    }})
 
-    page.on('response', (response) => {
-    console.log('<<', print_current_time(), response.status(), response.url().slice(0, 70))
-    })
+    // page.on('response', (response) => {
+    // console.log('<<', print_current_time(), response.status(), response.url().slice(0, 70))
+    // })
 
-    await page.reload();
-    // // wait for the page to load
-    // await page.waitForNavigation();
+    var loop = setInterval( async () => {      
 
-    // let page=await browser.newPage()
-    // await page.goto('https://blogweb.cn');
+        var now = Date.now();
+        if ( now > load_time ) {
 
-    // wait for xpath to load and click
-    const button = await page.waitForXPath('//*[@id="__next"]/div[1]/div/div[2]');
-    await button.click();
-    
-    // await page.close()
-    // await browser.close()
+            clearInterval(loop);
+            console.log( print_current_time() + ' time is up');
+
+            await page.reload();
+            console.log( print_current_time() + ' reloaded');
+            const button = await page.waitForXPath('//*[@id="submitBlock_1"]/div/div/div/div[3]');
+            await button.click();
+
+        } else if ( Math.floor( now/10 ) % 100 == 0 ) {
+            console.log(`${Math.floor(( load_time - now )/1000)} seconds left`);
+        }
+
+    }, 10);
+
 })()
 
 function print_current_time() {
@@ -51,9 +73,38 @@ function print_current_time() {
     var n = d.toLocaleTimeString();
     // milliseconds part from the timestamp
     var ms = d.getMilliseconds();
+    // make it 3 digits
+    ms = ('000' + ms).slice(-3);
     // console.log(n);
     return n + '.' + ms;
 }
+
+// close x:
+// /html/body/div[4]/div[2]/div
+
+
+    // await page.reload();
+    // // wait for the page to load
+    // await page.waitForNavigation();
+
+    // let page=await browser.newPage()
+    // await page.goto('https://blogweb.cn');
+
+    // wait for xpath to load and click
+    
+    
+    // await page.close()
+    // await browser.close()
+
+    
+// console.log(Math.floor( now/10 ))
+// console.log(Math.floor( now/10 ) % 100)
+// console.log(Math.floor( now/10 ) % 100 == 0)
+
+// if (now - last_time > 1000) {
+//     console.log(`${Math.floor((sell_time - now)/1000)} seconds left`);
+//     last_time = now;
+// }
 
 // await page.setRequestInterception(true)
 
